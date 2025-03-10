71a62b5115df9cc1cf814105a21def69f9eb0bfb
Maria Smirnova <92306135+MarieMittu@users.noreply.github.com>
2025-03-10 01:25:59 +0100

    grid view start inventory

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Transform photoGridContent;
+    //private List<SelectablePhoto> photos = new List<SelectablePhoto>();
+    public int photoColumns = 3;
+    private int currentGridIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
